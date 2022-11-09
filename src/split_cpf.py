13f13bc6ffdd256a38b6ba27aa93eba2ebe0d8a5
Renan Andrade <Fel9.renan02@gmail.com>
2022-11-08 22:19:13 -0300

    Added parameter type and length validation

--- src/split_cpf.py
+++ src/split_cpf.py
@@ -1,7 +1,10 @@
 from .remove_mask import remove_mask
 
 
-def split_cpf(cpf):
+def split_cpf(cpf: str):
     cpf = remove_mask(cpf)
-    body_digits, verification_digits = cpf[:9], cpf[9:-1]
+    if len(cpf) < 9 or len(cpf) > 11:  # without validation digits
+        raise ValueError("O cpf precisa ter 11 dígitos")
+    # 9 + (len(cpf) % 9) --> validation digits after cpf body
+    body_digits, verification_digits = cpf[:9], cpf[9 : 9 + (len(cpf) % 9)]
     return body_digits, verification_digits
