f2181cfab90a232591212f44886ce3d4e3f2b770
Renan Andrade <Fel9.renan02@gmail.com>
2022-11-10 20:50:16 -0300

    Removed comment; Altered error message

--- src/split_cpf.py
+++ src/split_cpf.py
@@ -3,8 +3,8 @@ from .remove_mask import remove_mask
 
 def split_cpf(cpf: str):
     cpf = remove_mask(cpf)
-    if len(cpf) < 9 or len(cpf) > 11:  # without validation digits
-        raise ValueError("O cpf precisa ter 11 dígitos")
-    # 9 + (len(cpf) % 9) --> validation digits after cpf body
+    if len(cpf) < 9 or len(cpf) > 11:
+        raise ValueError("O cpf precisa ter entre 9 a 11 dígitos")
+    # 9 + (len(cpf) % 9) --> validation digits after cpf's body
     body_digits, verification_digits = cpf[:9], cpf[9 : 9 + (len(cpf) % 9)]
     return body_digits, verification_digits
