feb0d10e987098f7a2a31ad1045b80757dbfdef0
Renan Andrade <Fel9.renan02@gmail.com>
2022-11-10 20:15:23 -0300

    Added length tests for generate cpf

--- tests/test_generate_cpf.py
+++ tests/test_generate_cpf.py
@@ -5,7 +5,7 @@ from ..src.validate_cpf import validate_cpf
 def test_generate_cpf():
     cpf = generate_cpf()
 
-    assert len(cpf[:-2]) == 9
-    assert len(cpf[9:]) == 2
+    assert len(cpf[:9]) == 9  # without validation digit
+    assert len(cpf[9:]) == 2  # validation digit
     assert cpf == validate_cpf(cpf)
     assert len(cpf) == 11
